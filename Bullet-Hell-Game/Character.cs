b8d3a8155eb192c52c8b18b396de35437d4fef3a
Bence Szucs <bence.szucs200625@gmail.com>
2024-05-31 11:36:55 -0400

    adds character class

---
+++ Bullet-Hell-Game/Character.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Bullet_Hell_Game
+{
+    public class Character
+    {
+        private AnimatedSprite sprite;
+
+        public Vector2 MoveVelocity {  get; set; }
+
+        public Vector2 Position { get; set; }
+
+        public Character(AnimatedSprite sprite) : this(Vector2.Zero, Vector2.Zero, sprite) { }
+
+        public Character(Vector2 position, Vector2 velocity, AnimatedSprite sprite)
+        {
+            MoveVelocity = velocity;
+            Position = position;
+            this.sprite = sprite;
+        }
+
+        public void Move()
+        {
+            Position = Vector2.Add(Position, MoveVelocity);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Move();
+        }
+
+        public void Draw(SpriteBatch spriteBatch, bool horizontalFlip)
+        {
+            sprite.Draw(spriteBatch, Position, horizontalFlip);
+        }
+    }
+}
