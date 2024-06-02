f57bd6d07081df581f51af7f6c07149c82dea8af
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-02 14:58:12 -0400

    Adds ICollidable interface

---
+++ Bullet-Hell-Game/ICollidable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bullet_Hell_Game
+{
+    public interface ICollidable
+    {
+        bool IsCollidable { get; }
+        Rectangle BoundingBox { get; }
+        bool IsColliding(Rectangle rect);
+        void OnCollision();
+    }
+}
