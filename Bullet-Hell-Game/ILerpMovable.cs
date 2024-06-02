282ce1cfdc63c67ff1505307557c8407c55bcf3a
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-02 15:34:52 -0400

    Creates interface for movable objects with Lerp movement

---
+++ Bullet-Hell-Game/ILerpMovable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bullet_Hell_Game
+{
+    public interface ILerpMovable
+    {
+        Vector2 Position { get; }
+        Vector2 PreviousPosition { get; }
+    }
+}
