51ce33be58db863eccff2ff047cf35ec19db24a6
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-02 15:47:56 -0400

    Adds lerp position and lerp draw to ILerpMovable

--- Bullet-Hell-Game/ILerpMovable.cs
+++ Bullet-Hell-Game/ILerpMovable.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,7 @@ namespace Bullet_Hell_Game
     {
         Vector2 Position { get; }
         Vector2 PreviousPosition { get; }
+        Vector2 LerpPosition { get; }
+        void LerpDraw(SpriteBatch spriteBatch, float ALPHA);
     }
 }
