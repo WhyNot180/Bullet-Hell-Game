49e44002a1b7382c5759a890c37620c344134cbf
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-04 11:32:42 -0400

    Adds collision area to manage collisions

---
+++ Bullet-Hell-Game/CollisionArea.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bullet_Hell_Game
+{
+    public class CollisionArea
+    {
+        public enum CollisionType
+        {
+            EnemyProjectile,
+            PlayerProjectile,
+            Obstacle,
+            Enemy,
+            Player
+        }
+
+        public List<ICollidable> colliders = new List<ICollidable>();
+
+        private QuadTree quadTree;
+
+        public CollisionArea(Rectangle bounds)
+        {
+            quadTree = new QuadTree(0, bounds);
+        }
+
+        public void Update()
+        {
+            quadTree.Clear();
+            colliders.ForEach(collider => quadTree.Insert(collider));
+
+            List<ICollidable> returnColliders = new List<ICollidable>();
+            foreach (var collider in colliders)
+            {
+                returnColliders.Clear();
+                quadTree.Retrieve(returnColliders, collider.BoundingBox);
+                foreach (var returnCollider in returnColliders)
+                {
+                    if (IsColliding(collider.BoundingBox, returnCollider.BoundingBox))
+                    {
+                        returnCollider.OnCollision(collider.CollisionType);
+                        collider.OnCollision(collider.CollisionType);
+                    }
+                }
+            }
+
+        }
+
+        public bool IsColliding(Rectangle collisionRectA, Rectangle collisionRectB)
+        {
+
+        }
+    }
+}
