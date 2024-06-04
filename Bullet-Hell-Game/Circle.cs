b1cc544391d9ef8bf9b3c1c4e71a3285b322a4ed
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-04 18:44:06 -0400

    Adds circle struct

---
+++ Bullet-Hell-Game/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bullet_Hell_Game
+{
+    public struct Circle
+    {
+        public int x;
+        public int y;
+        public int radius;
+
+        public Circle(int x, int y, int radius)
+        {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+    }
+}
