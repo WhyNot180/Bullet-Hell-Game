a21115433a512b3591e44d6bba43fce2c26e6421
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-05 20:45:01 -0400

    Makes player collidable

--- Bullet-Hell-Game/Player.cs
+++ Bullet-Hell-Game/Player.cs
@@ -1,18 +1,38 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bullet_Hell_Game
 {
-    public class Player : Character
+    public class Player : Character, ICollidable
     {
-        private int lives;
+        private int lives = 3;
 
-        public Player(AnimatedSprite sprite, Vector2 position, float speed) : base(sprite, position, Vector2.Zero, speed) { }
+        public CollisionArea.CollisionType CollisionType { get; } = CollisionArea.CollisionType.Player;
 
+        public RotatableShape BoundingBox { get; private set; }
+        public bool IsCollidable { get; set; }
+
+        public Player(AnimatedSprite sprite, Vector2 position, float speed) : base(sprite, position, Vector2.Zero, speed) 
+        {
+            BoundingBox = new RotatableShape(new Rectangle((int) Math.Round(position.X), (int) Math.Round(position.Y), (int) Math.Round(sprite.Texture.Width * sprite.Scale*1.5), (int) Math.Round(sprite.Texture.Height * sprite.Scale*1.5)), 0);
+        }
+
+        public override void Update(float deltaSeconds)
+        {
+            base.Update(deltaSeconds);
+            BoundingBox.X = (int) Math.Round(Position.X);
+            BoundingBox.Y = (int) Math.Round(Position.Y);
+        }
+
+        public void OnCollision(CollisionArea.CollisionType collisionType)
+        {
+            Debug.WriteLine("Player Collision: " + collisionType.ToString());
+        }
 
     }
 }
