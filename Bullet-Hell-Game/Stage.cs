6492dbba1db35d3d9f7dac69632a4906c29b557a
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-07 10:43:06 -0400

    Adds stage class

---
+++ Bullet-Hell-Game/Stage.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Bullet_Hell_Game
+{
+    public class Stage : ILerpMovable
+    {
+        public Vector2 Position {  get; set; }
+        public Vector2 PreviousPosition { get; private set; } = Vector2.Zero;
+        public Vector2 LerpPosition { get; private set; } = Vector2.Zero;
+        public Vector2 MoveVelocity { get; set; } = Vector2.Zero;
+        public float Speed { get; set; }
+
+        private Texture2D background;
+
+        public Stage(Texture2D backgroundSprite)
+        {
+            background = backgroundSprite;
+        }
+
+        public void Move(float deltaSeconds)
+        {
+            Position = Vector2.Add(Position, Vector2.Multiply(MoveVelocity, deltaSeconds * Speed));
+        }
+
+        public void Update(float deltaTime)
+        {
+            PreviousPosition = Position;
+            Move(deltaTime);
+        }
+
+        public void LerpDraw(SpriteBatch spriteBatch, float ALPHA)
+        {
+            LerpPosition = Vector2.Lerp(PreviousPosition, Position, ALPHA);
+            spriteBatch.Draw(background, LerpPosition, Color.Transparent);
+        }
+
+    }
+}
