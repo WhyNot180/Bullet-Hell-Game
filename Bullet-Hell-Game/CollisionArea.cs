0f728c784405e079a00987e583e7b0c6c2b0b3c8
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-15 17:26:47 -0400

    Adds collision checked bool to prevent double collisions and fixes normal vector directions

--- Bullet-Hell-Game/CollisionArea.cs
+++ Bullet-Hell-Game/CollisionArea.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bullet_Hell_Game
 {
@@ -38,11 +34,12 @@ namespace Bullet_Hell_Game
             {
                 if (collider.IsCollidable)
                 {
+                    collider.CollisionChecked = true;
                     returnColliders.Clear();
                     quadTree.Retrieve(returnColliders, collider.BoundingBox);
                     foreach (var returnCollider in returnColliders)
                     {
-                        if (!returnCollider.Equals(collider) && returnCollider.IsCollidable)
+                        if (!returnCollider.Equals(collider) && returnCollider.IsCollidable && !returnCollider.CollisionChecked)
                         {
                             if (IsColliding(collider.BoundingBox, returnCollider.BoundingBox, out Vector2 minTranslationVect))
                             {
@@ -53,6 +50,7 @@ namespace Bullet_Hell_Game
                     }
                 }
             }
+            colliders.ForEach(collider => collider.CollisionChecked = false);
 
         }
 
@@ -60,7 +58,7 @@ namespace Bullet_Hell_Game
         {
             minimumTranslationVector = Vector2.Zero;
 
-            float overlap = float.PositiveInfinity;
+            float overlap = float.MaxValue;
             Vector2 overlapDirection = Vector2.Zero;
 
             Vector2[] axesA = GetAxes(collisionShapeA);
@@ -72,13 +70,13 @@ namespace Bullet_Hell_Game
 
                 Vector2 projectionA = MinMaxProjectVector(collisionShapeA, axis);
                 Vector2 projectionB = MinMaxProjectVector(collisionShapeB, axis);
-                
-                if (projectionA.X > projectionB.Y || projectionB.X > projectionA.Y)
+
+                if (projectionA.X >= projectionB.Y || projectionB.X >= projectionA.Y)
                 {
                     return false;
                 } else
                 {
-                    float projectionOverlap = (projectionB.X - projectionA.Y) - (projectionA.X - projectionB.Y);
+                    float projectionOverlap = MathF.Min(projectionB.Y - projectionA.X, projectionA.Y - projectionB.X);
 
                     if (projectionOverlap < overlap)
                     {
@@ -94,13 +92,13 @@ namespace Bullet_Hell_Game
 
                 Vector2 projectionA = MinMaxProjectVector(collisionShapeA, axis);
                 Vector2 projectionB = MinMaxProjectVector(collisionShapeB, axis);
-                
-                if (projectionA.X > projectionB.Y || projectionB.X > projectionA.Y)
+
+                if (projectionA.X >= projectionB.Y || projectionB.X >= projectionA.Y)
                 {
                     return false;
                 } else
                 {
-                    float projectionOverlap = (projectionB.X - projectionA.Y) - (projectionA.X - projectionB.Y);
+                    float projectionOverlap = MathF.Min(projectionB.Y - projectionA.X, projectionA.Y - projectionB.X);
 
                     if (projectionOverlap < overlap)
                     {
@@ -108,6 +106,13 @@ namespace Bullet_Hell_Game
                         overlapDirection = axis;
                     }
                 }
+            }
+
+            Vector2 centerCollidersAB = new Vector2(collisionShapeB.X  - collisionShapeA.X, collisionShapeB.Y - collisionShapeA.Y);
+
+            if (Vector2.Dot(centerCollidersAB, overlapDirection) > 0f)
+            {
+                overlapDirection = -overlapDirection;
             }
 
             minimumTranslationVector = Vector2.Multiply(overlapDirection, overlap);
@@ -118,21 +123,25 @@ namespace Bullet_Hell_Game
 
         private Vector2 MinMaxProjectVector(RotatableShape collisionShape, Vector2 axis)
         {
-            List<float> projections = new List<float>();
-            collisionShape.Vertices.ForEach(vert =>  projections.Add(Vector2.Dot(axis, new Vector2(vert.X + collisionShape.X, vert.Y + collisionShape.Y))));
-            
-            double min = projections.Min();
-            double max = projections.Max();
+            float min = float.MaxValue;
+            float max = float.MinValue;
+
+            for (int i = 0; i < collisionShape.AbsoluteVertices.Count; i++)
+            {
+                float projection = Vector2.Dot(collisionShape.AbsoluteVertices[i], axis);
+                if (projection < min) { min = projection; }
+                else if (projection > max) { max = projection; }
+            }
 
             return new Vector2((float) min, (float) max);
         }
 
         private Vector2[] GetAxes(RotatableShape collisionShape)
         {
-            Vector2[] axes = new Vector2[collisionShape.Vertices.Count];
+            Vector2[] axes = new Vector2[collisionShape.RelativeVertices.Count];
             for (int i = 0; i < axes.Length; i++)
             {
-                Vector2 sideVect = Vector2.Subtract(collisionShape.Vertices[i], collisionShape.Vertices[i + 1 == collisionShape.Vertices.Count ? 0 : i + 1]);
+                Vector2 sideVect = Vector2.Subtract(collisionShape.RelativeVertices[(i + 1) % axes.Length], collisionShape.RelativeVertices[i]);
                 axes[i] = new Vector2(-sideVect.Y, sideVect.X);
                 axes[i].Normalize();
             }
