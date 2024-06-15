5153a5aac9d5d28c3b561e393ce00003962a4670
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-15 17:28:35 -0400

    fixes player bounding box positioning

--- Bullet-Hell-Game/Player.cs
+++ Bullet-Hell-Game/Player.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
@@ -16,11 +17,20 @@ namespace Bullet_Hell_Game
         public CollisionArea.CollisionType CollisionType { get; } = CollisionArea.CollisionType.Player;
 
         public RotatableShape BoundingBox { get; private set; }
-        public bool IsCollidable { get; set; }
+        public bool IsCollidable { get; set; } = true;
+        public bool CollisionChecked { get; set; } = false;
 
         public Player(AnimatedSprite sprite, Vector2 position, float speed) : base(sprite, position, Vector2.Zero, speed) 
         {
-            BoundingBox = new RotatableShape(new Rectangle((int) Math.Round(position.X), (int) Math.Round(position.Y), (int) Math.Round(sprite.Texture.Width * sprite.Scale*1.5), (int) Math.Round(sprite.Texture.Height * sprite.Scale*1.5)), 0);
+            float width = sprite.Texture.Width * sprite.Scale;
+            float height = sprite.Texture.Height * sprite.Scale;
+            BoundingBox = new RotatableShape(position.X + (width/2), position.Y + (height/2), width, height, 0, new List<Vector2>
+            {
+                new(-width / 2, height / 2),
+                new(width / 2, height / 2),
+                new(width / 2, -height / 2),
+                new(-width / 2, -height / 2)
+            });
         }
 
         public override void Update(float deltaSeconds)
@@ -56,16 +66,16 @@ namespace Bullet_Hell_Game
             {
                 MoveVelocity = Vector2.Normalize(MoveVelocity);
             }
-
             base.Update(deltaSeconds);
-            BoundingBox.X = (int) Math.Round(Position.X);
-            BoundingBox.Y = (int) Math.Round(Position.Y);
+            BoundingBox.Move(new Vector2(BoundingBox.X, BoundingBox.Y) + MoveVelocity*Speed);
+
         }
 
         public void OnCollision(CollisionArea.CollisionType collisionType, Vector2 minimumTranslationVector)
         {
-            Debug.WriteLine("Player Collision: " + collisionType.ToString());
-            Position = Vector2.Add(Position, minimumTranslationVector);
+            Position = Vector2.Add(Position, minimumTranslationVector/2);
+            BoundingBox.Move(new Vector2(BoundingBox.X, BoundingBox.Y) + minimumTranslationVector / 2);
+            MoveVelocity = Vector2.Zero;
         }
 
     }
