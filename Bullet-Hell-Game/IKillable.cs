f93c48ecffb28593af9c8dfade13123d87dd73aa
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-17 18:53:26 -0400

    Creates IKillable interface

---
+++ Bullet-Hell-Game/IKillable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bullet_Hell_Game
+{
+    public interface IKillable
+    {
+        void OnKill(EventArgs e);
+        event EventHandler? Kill;
+    }
+}
