a599015294354e34e607cfa9f06c720672f39a07
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-17 19:31:41 -0400

    Adds FixedUpdatable interface

---
+++ Bullet-Hell-Game/IFixedUpdatable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bullet_Hell_Game
+{
+    public interface IFixedUpdatable : IKillable
+    {
+        void FixedUpdate();
+    }
+}
