808179c94284e74e3c4e0750958e0e350c44ae32
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-19 17:37:22 -0400

    Adds comments to IFixedUpdatable interface

--- Bullet-Hell-Game/IFixedUpdatable.cs
+++ Bullet-Hell-Game/IFixedUpdatable.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Bullet_Hell_Game
 {
+    /// <summary>
+    /// Used by objects that must be updated at a fixed framerate
+    /// </summary>
     public interface IFixedUpdatable : IKillable
     {
         void FixedUpdate();
