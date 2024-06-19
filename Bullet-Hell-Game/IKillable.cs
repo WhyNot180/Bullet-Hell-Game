4cdee248bfe863d93219474d96e3ebbc6dbffc60
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-19 17:37:33 -0400

    Adds comments to IKillable interface

--- Bullet-Hell-Game/IKillable.cs
+++ Bullet-Hell-Game/IKillable.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace Bullet_Hell_Game
 {
+    /// <summary>
+    /// Used for any objects that might not be automatically collected by the garbage collector.
+    /// I.e. any object that is in a list not accessible directly by the object
+    /// </summary>
     public interface IKillable
     {
+        /// <summary>
+        /// Call when killing a killable entity
+        /// </summary>
+        /// <param name="e"></param>
         void OnKill(EventArgs e);
+
         event EventHandler? Kill;
     }
 }
