1942b2de36fd1115b1d0597deca75cb407dab426
Bence Szucs <bence.szucs200625@gmail.com>
2024-06-19 17:38:52 -0400

    Adds comments to StageElement class

--- Bullet-Hell-Game/StageElement.cs
+++ Bullet-Hell-Game/StageElement.cs
@@ -5,20 +5,33 @@ using System.Diagnostics;
 
 namespace Bullet_Hell_Game
 {
+    /// <summary>
+    /// A collidable entity belonging to a Stage; typically an obstacle such as a wall
+    /// </summary>
     public class StageElement : ILerpMovable, ICollidable, IFixedUpdatable
     {
         public Vector2 Position { get; set; }
         public Vector2 PreviousPosition { get; private set; } = Vector2.Zero;
         public Vector2 LerpPosition { get; private set; } = Vector2.Zero;
         public Vector2 MoveVelocity { get; set; } = Vector2.Zero;
+
         public bool IsCollidable { get; set; }
         public RotatableShape BoundingBox { get; set; }
         public CollisionArea.CollisionType CollisionType { get; set; }
         public bool CollisionChecked { get; set; } = false;
+
         private Texture2D texture;
 
         public event EventHandler? Kill;
 
+        /// <summary>
+        /// Initializes a Stage Element with a texture, position, and collision box
+        /// </summary>
+        /// <param name="texture"></param>
+        /// <param name="isCollidable"></param>
+        /// <param name="boundingBox"></param>
+        /// <param name="position"></param>
+        /// <param name="collisionType"></param>
         public StageElement(Texture2D texture, bool isCollidable, RotatableShape boundingBox, Vector2 position, CollisionArea.CollisionType collisionType)
         {
             this.texture = texture;
